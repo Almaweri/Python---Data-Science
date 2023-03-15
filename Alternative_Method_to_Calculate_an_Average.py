fb04c1dae530fdcdcab973f198968eff1ff3afa4
Maged Almaweri <maged.almaweri@yahoo.com>
2023-03-15 00:44:44 -0400

    Rating, Sum, Average in python

---
+++ Alternative_Method_to_Calculate_an_Average.py
@@ -0,0 +1,19 @@
+row_1 = ['Facebook', 0.0, 'USD', 2974676, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+row_3 = ['Clash of Clans', 0.0, 'USD', 2130805, 4.5]
+row_4 = ['Fruit Ninja Classic', 1.99, 'USD', 698516, 4.5]
+row_5 = ['Minecraft: Pocket Edition', 6.99, 'USD', 522012, 4.5]
+
+app_data_set = [row_1, row_2, row_3, row_4, row_5]
+
+rating_sum = []
+
+for row in app_data_set :
+    rating = (row[-1])
+    rating_sum.append(rating)
+print(rating_sum)  
+ 
+avg_rating = sum(rating_sum) / len(rating_sum)
+    
+print(avg_rating)
+#print(empty_list)
