ad3ed1e30dd131044560d2fd60b2de00975c34b8
Maged Almaweri <maged.almaweri@yahoo.com>
2023-03-14 22:56:02 -0400

    Correcting the example

--- Dealing_with_Errors.py
+++ Dealing_with_Errors.py
@@ -6,8 +6,13 @@ apps_data = list(read_file)
 rating_sum = 0
 
 for row in apps_data[1:]:
+   # print(len(row))
     rating = float(row[7])
     rating_sum = rating_sum + rating
     avg_rating = rating_sum / len(apps_data[1:])
-    print(avg_rating)
+    nah = len(apps_data[1:])
+    #print(nah)
+    avg_rating2 = rating_sum / 7197
+print(avg_rating)
+print(avg_rating2)
     
