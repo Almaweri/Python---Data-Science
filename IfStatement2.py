7e97ca1ee56f0e51280039a745b721a12c7dd63e
Maged Almaweri <maged.almaweri@yahoo.com>
2023-03-16 21:11:13 -0400

    If Statement

---
+++ IfStatement2.py
@@ -0,0 +1,16 @@
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+free_apps_ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    price = float(row[4])
+    
+    if price == 0.0:
+        free_apps_ratings.append(rating)
+avg_rating_free = sum(free_apps_ratings) / len(free_apps_ratings)
+print(avg_rating_free)
+
+
---
+++ If_Statement.py
@@ -0,0 +1,19 @@
+open_file = open('D:/Python/PthonRep/Python---Data-Science/AppleStore.csv', encoding='utf-8')
+from csv import reader
+read_file = reader(open_file)
+apps_data = list(read_file)
+
+free_app = []
+non_free = []
+
+for row in apps_data[1:]:
+    rating = float(row[7])
+    price = float(row[7])
+    #ratings.append(rating)
+    
+    if price == 0:
+        free_app.append(rating)
+    else:
+        non_free.append(rating)
+print(free_app)
+print(non_free)
