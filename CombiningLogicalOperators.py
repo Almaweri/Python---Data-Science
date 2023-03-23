326fd1a132b68ed28208d884f93ab3409d04077c
Maged Almaweri <maged.almaweri@yahoo.com>
2023-03-22 22:18:55 -0400

    New

--- CombiningLogicalOperators.py
+++ CombiningLogicalOperators.py
@@ -1,3 +1,8 @@
+open_file = open('D:/Python/PthonRep/Python---Data-Science/AppleStore.csv', encoding='utf-8')
+from csv import reader
+read_file = reader(open_file)
+apps_data = list(read_file)
+
 n_of_apps = 0
 
 for row in apps_data[1:]:
