0b689b9ebf6b5211ba730621dc807b5bc4dbf85a
Maged Almaweri <maged.almaweri@yahoo.com>
2023-05-16 23:51:48 -0400

    Updating the code

--- TheElifClause.py
+++ TheElifClause.py
@@ -1,15 +1,23 @@
 open_file = open('D:/Python/PthonRep/Python---Data-Science/AppleStore.csv', encoding='utf-8')
-apps_data = list(open_file)
-for app in apps_data:
-    price = app[7]
+from csv import reader
+data_in_csv = reader(open_file)
+apps_data = list(data_in_csv)
 
-    if price == 0.0:
-        app.append('free')
-    if price > 0.0 and price < 20:
-        app.append('affordable')
-    if price >= 20 and price < 50:
-        app.append('expensive')
-    if price >= 50:
-        app.append('very expensive')
+for app in apps_data[1:]:
+    try:
+        price = app[5]
+        price = float(price)
+        if price == 0.0:
+            app.append('free')
+        elif price > 0.0 and price < 20.0:
+            app.append('affordable')
+        elif price >= 20 and price < 50:
+            app.append('expensive')
+        elif price >= 50:
+            app.append('very expensive')
+    except ValueError:
+        print("Invalid price value")        
 
-print(apps_data[0:5])
+for row in apps_data[0:4]:
+    print("Price is: --->>>   ",  price)
+    print(row)
