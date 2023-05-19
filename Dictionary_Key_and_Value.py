ec1848204bce3aafc9ba76c8b05f2418f96d90a3
Maged Almaweri <maged.almaweri@yahoo.com>
2023-05-18 22:11:49 -0400

    Apps Rating Using Dictionary

---
+++ Dictionary_Key_and_Value.py
@@ -0,0 +1,10 @@
+d_1 = {'key_1': 'value_1',
+       'key_2': 1,
+       'key_3': 1.832,
+       'key_4': False,
+       'key_5': [1,2,3],
+       'key_6': {'inner_key' : 10}}
+
+print(d_1)
+print(d_1['key_1'])
+print(d_1['key_6'])
