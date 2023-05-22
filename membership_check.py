9601259729df76862704028773d47c6c0ce399b6
Maged Almaweri <maged.almaweri@yahoo.com>
2023-05-21 22:44:12 -0400

    Checking for Membership

---
+++ membership_check.py
@@ -0,0 +1,8 @@
+content_ratings = {'4+': 4433, '9+': 987, '12+': 1155, '17+': 622}
+
+is_in_dictionary_1 = '9+' in content_ratings
+is_in_dictionary_2 = 987 in content_ratings
+
+if '17+' in content_ratings:
+    result = 'It exists'
+print(result)
