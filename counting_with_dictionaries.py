880018a2cb1edbca4d552186cb9935db05f85e42
Maged Almaweri <maged.almaweri@yahoo.com>
2023-05-22 22:22:34 -0400

    updating the counts

--- counting_with_dictionaries.py
+++ counting_with_dictionaries.py
@@ -3,8 +3,7 @@ ratings = ['4+', '4+', '4+', '9+', '9+', '12+', '17+']
 
 for c_rating in ratings:
     if c_rating in content_ratings:
-        print('each rating is: ', content_ratings[c_rating])
-    print(content_ratings)
+        print(content_ratings)
     
 print('Final dictionary:')
 print(content_ratings)
