71710a9fc15cf312654b2aa60514f89f448de006
Maged Almaweri <maged.almaweri@yahoo.com>
2023-05-24 00:46:58 -0400

    format() method used with strings in Python

---
+++ string_format_method.py
@@ -0,0 +1,16 @@
+animal = 'dog'
+action = 'bit'
+print("does your {} {}?".format(animal, action))
+
+maria_string = 'Maria loves {} and {}.'
+print(maria_string.format('Math', 'statistics'))
+
+print("Moahmmed has {} balloons.".format(27))
+
+first_name = "Alice"
+last_name = "Smith"
+print("Hello, {} {}!".format(first_name, last_name))
+
+#You can even use keyword arguments for more clarity:
+print("Hello, {first_name} {last_name}!".format(first_name="Alice", last_name="Smith"))
+
