b7e51abdb3a69b7cfd3434b7ac8867645fc5fb94
Maged Almaweri <maged.almaweri@yahoo.com>
2023-06-13 23:47:50 -0400

    Quiz: MIN, MAX, & AVG

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -263,3 +263,42 @@ SELECT MIN(o.standard_qty) AS standard_min,
        MAX(o.poster_qty) AS poster_max
 FROM orders o;
 
+-- Quiz: MIN, MAX, & AVG
+
+-- 1- When was the earliest order ever placed? You only need to return the date.
+SELECT MIN(o.occurred_at) as min_date
+FROM orders o;
+
+-- 2- Try performing the same query as in question 1 without using an aggregation function.
+SELECT o.occurred_at as min_date
+FROM orders o
+order by o.occurred_at ASC
+limit 1;
+
+-- 3- When did the most recent (latest) web_event occur?
+SELECT MAX(w.occurred_at) as latest_web_event
+FROM web_events w;
+
+-- 4- Try to perform the result of the previous query without using an aggregation function.
+SELECT w.occurred_at as latest_web_event
+FROM web_events w
+order by w.occurred_at DESC
+limit 1;
+
+-- 5- Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+SELECT AVG(total) as total,
+       AVG(poster_amt_usd) as poster_amt_usd,
+       AVG(standard_amt_usd) as standard_amt_usd,
+       AVG(gloss_amt_usd) as gloss_amt_usd,
+       AVG(total_amt_usd) as total_amt_usd      
+FROM orders;
+
+-- 6- Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders?
+
+SELECT *
+FROM (SELECT total_amt_usd
+   FROM orders
+   ORDER BY total_amt_usd
+   LIMIT 3457) AS Table1
+ORDER BY total_amt_usd DESC
+LIMIT 2;
