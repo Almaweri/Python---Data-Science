81f8d38e53c480d4109faeb8f5309e7d15dc03b1
Maged Almaweri <maged.almaweri@yahoo.com>
2023-06-13 23:12:10 -0400

    SUM Questions and Answers

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -222,3 +222,35 @@ FROM accounts a;
 SELECT * 
 FROM accounts
 WHERE primary_poc IS NULL;
+
+
+-- SUM
+SELECT SUM(o.standard_qty) AS standard,
+       SUM(o.gloss_qty) AS gloss,
+       SUM(o.poster_qty) AS poster
+FROM orders o;
+
+-- Quiz: SUM
+
+-- 1- Find the total amount of poster_qty paper ordered in the orders table.
+SELECT SUM(o.poster_qty) as poster
+FROM orders o;
+
+-- 2- Find the total amount of standard_qty paper ordered in the orders table.
+SELECT SUM(o.standard_qty)
+FROM orders o;
+
+-- 3- Find the total dollar amount of sales using the total_amt_usd in the orders table.
+SELECT SUM(o.total_amt_usd) as total_amt_usd
+FROM orders o;
+
+-- 4- Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table.
+SELECT SUM(o.total_amt_usd) as total_amt_usd, SUM(o.gloss_amt_usd) as gloss_amt_usd
+FROM orders o;
+
+-- 5- Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both aggregation and a mathematical operator.
+SELECT SUM(o.standard_amt_usd) / SUM(o.standard_qty) as tandard_amt_usd_per_unit 
+FROM orders o;
+
+
+
