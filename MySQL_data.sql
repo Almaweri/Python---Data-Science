7cc4f9e9cad7257020bec390a4eef68c41c8dde4
Maged Almaweri <maged.almaweri@yahoo.com>
2023-06-22 21:51:13 -0400

    HAVING Q 9 and 10

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -559,3 +559,22 @@ GROUP BY a.id, a.name, w.channel
 HAVING COUNT(*) > 6
 ORDER BY use_of_channel;
 
+-- 9- Which account used facebook most as a channel?
+SELECT a.id, a.name, w.channel, COUNT(*) AS use_of_channel
+FROM accounts a
+JOIN web_events w ON a.id = w.account_id
+WHERE w.channel = 'facebook'
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+limit 1;
+
+-- 10- Which channel was most frequently used by most accounts?
+
+SELECT a.id, a.name, w.channel, COUNT(*) AS use_of_channel
+FROM accounts a
+JOIN web_events w ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+limit 10;
+
+
