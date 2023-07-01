121fa26e39265ac449c904bc976d4cedd7ffc812
Maged Almaweri <maged.almaweri@yahoo.com>
2023-06-30 23:07:40 -0400

    QUIZ CASE  Q2

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -768,5 +768,21 @@ FROM orders o
 GROUP by 1,2;
 
 
+-- 2- Write a query to display the number of orders in each of three categories, based on the total number of items in each order.
+-- The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+
+SELECT 
+    CASE
+        WHEN o.total >= 2000 THEN 'At Least 2000' 
+        WHEN o.total <= 2000 AND o.total >= 1000 THEN 'Between 1000 and 2000' 
+        WHEN o.total < 1000 THEN 'Less than 1000' 
+        ELSE 'unknown'
+    END AS order_level,
+    COUNT(*) as total_orders
+FROM orders o 
+GROUP BY 1;
+
+
+
 
 
