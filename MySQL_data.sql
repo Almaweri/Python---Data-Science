c7ab53eb3b8f4ad388da99473bd37b24bc444313
Maged Almaweri <maged.almaweri@yahoo.com>
2023-08-03 22:21:41 -0400

    region with the largest(sum)of sales amt used

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -1082,3 +1082,31 @@ FROM
     SalesData
 WHERE
     channel_rank = 1;
+
+
+-- Showing the orders with the hightest first 
+
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+
+FROM(SELECT region_name, MAX(total_amt) total_amt
+
+     FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+             FROM sales_reps s
+             JOIN accounts a ON a.sales_rep_id = s.id
+             JOIN orders o   ON o.account_id = a.id
+             JOIN region r   ON r.id = s.region_id
+             GROUP BY 1, 2) t1
+     GROUP BY 1) t2
+     
+JOIN (
+    
+    SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+     FROM sales_reps s
+     JOIN accounts a ON a.sales_rep_id = s.id
+     JOIN orders o   ON o.account_id = a.id
+     JOIN region r   ON r.id = s.region_id
+     GROUP BY 1,2
+     ORDER BY 3 DESC) t3
+     
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt
+Order by total_amt DESC;
