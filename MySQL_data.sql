e09b61842e54b8fe4bdf56beb0cac9efeb93a060
Maged Almaweri <maged.almaweri@yahoo.com>
2023-08-16 23:09:30 -0400

    What is the lifetime average amount spent in terms of total_amt_usd, including only the companies that spent more per order, on average, than the average of all orders

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -1248,3 +1248,16 @@ WHERE a.id IN (
 )
 GROUP BY acct_id, acct_name
 ORDER BY lifetime_avg_amount_spent DESC;
+
+-- 6- What is the lifetime average amount spent in terms of total_amt_usd,
+--including only the companies that spent more per order, on average, than the average of all orders?
+
+		
+SELECT AVG(avg_amt)
+FROM (SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+    FROM orders o
+    GROUP BY 1
+    HAVING AVG(o.total_amt_usd) > (SELECT AVG(o.total_amt_usd) avg_all
+FROM orders o)) temp_table;
+
+
