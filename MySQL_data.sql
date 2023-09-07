d4a91592507934f12bafbe99d4cc0d36ba171315
Maged Almaweri <maged.almaweri@yahoo.com>
2023-09-06 22:27:09 -0400

    For the account that purchased the most (in total over their lifetime as a customer) standard_qty paper, how many accounts still had more in total purchases?

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -1277,7 +1277,8 @@ ORDER BY 2 desc;
 
 
 -- Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
-WITH t1 AS (
+-- Start with T1 as table one 
+WITH t1 AS (  
   SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
    FROM sales_reps s
    JOIN accounts a
@@ -1288,11 +1289,35 @@ WITH t1 AS (
    ON r.id = s.region_id
    GROUP BY 1,2
    ORDER BY 3 DESC), 
+   -- add another table to WITH statment
 t2 AS (
    SELECT region_name, MAX(total_amt) total_amt
    FROM t1
    GROUP BY 1)
+   -- Now you do the SELECT statment to get the data from both tables with JOIN
 SELECT t1.rep_name, t1.region_name, t1.total_amt
 FROM t1
 JOIN t2
 ON t1.region_name = t2.region_name AND t1.total_amt = t2.total_amt;
+
+
+
+-- For the account that purchased the most (in total over their lifetime as a customer) standard_qty paper, how many accounts still had more in total purchases?
+
+WITH t1 AS (
+  SELECT a.name account_name, SUM(o.standard_qty) total_std, SUM(o.total) total
+  FROM accounts a
+  JOIN orders o
+  ON o.account_id = a.id
+  GROUP BY 1
+  ORDER BY 2 DESC
+  LIMIT 1), 
+t2 AS (
+  SELECT a.name
+  FROM orders o
+  JOIN accounts a
+  ON a.id = o.account_id
+  GROUP BY 1
+  HAVING SUM(o.total) > (SELECT total FROM t1))
+SELECT COUNT(*)
+FROM t2;
