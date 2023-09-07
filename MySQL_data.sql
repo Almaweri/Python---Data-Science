5cc07af9b1f19c678f7c59050ded0c2731ec16ad
Maged Almaweri <maged.almaweri@yahoo.com>
2023-09-06 23:05:50 -0400

    Inline Query

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -1332,3 +1332,16 @@ IN (SELECT DISTINCT student_id
     FROM gpa_table
     WHERE gpa>3.5
     );
+
+
+    -- Inline Query
+SELECT dept_name,
+       max_gpa
+FROM department_db x
+     (SELECT dept_id
+             MAX(gpa) as max_gpa
+      FROM students
+      GROUP BY dept_id
+      )y
+WHERE x.dept_id = y.dept_id
+ORDER BY dept_name;
