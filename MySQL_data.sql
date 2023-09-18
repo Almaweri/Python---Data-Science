8351098de321dd677472b10752839ebf987c4d08
Maged Almaweri <maged.almaweri@yahoo.com>
2023-09-17 23:04:26 -0400

    Write a query to change the date into the correct SQL date format. You will need to use at least SUBSTR and CONCAT to perform this operation.

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -1449,3 +1449,10 @@ WITH T1 AS (
 )
 SELECT CONCAT(T1.ACCOUNT_ID, '_', T1.CHANNEL, '_', COUNT)
 FROM T1;
+
+
+
+-- 4. Write a query to change the date into the correct SQL date format. You will need to use at least SUBSTR and CONCAT to perform this operation.
+
+SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2)) new_date
+FROM sf_crime_data;
