8de3ee2cd42fcae9198d4f56794cf93cb641cf03
Maged Almaweri <maged.almaweri@yahoo.com>
2023-10-09 23:19:41 -0400

    3rd Advanced Functions: Aliases for Multiple Window Functions

--- MySQL_data.sql
+++ MySQL_data.sql
@@ -1761,3 +1761,18 @@ FROM   amazon_sales_db
 WHERE  order_date < '2017-01-01'
 WINDOW monthly_window AS
        (PARTITION BY month(order_date) ORDER BY order_date);
+
+-- 3rd example:
+
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER account_year_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders 
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at))
